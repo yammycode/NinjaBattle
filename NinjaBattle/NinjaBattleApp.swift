996676f77787df62c978a7825b79c5137b3727d7
Anton <info@yammycode.com>
2022-11-04 14:57:10 +0300

    Initial Commit

---
+++ NinjaBattle/NinjaBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinjaBattleApp.swift
+//  NinjaBattle
+//
+//  Created by Anton Saltykov on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NinjaBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
